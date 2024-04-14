830a084aad0944a49303f94e5eee8a59058d6798
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-04-14 15:09:36 +1000

    minor bugfixes

--- view/ViewSearchBox.php
+++ view/ViewSearchBox.php
@@ -5,7 +5,7 @@ class ViewSearchBox
     {
         ?>
         <form action="index.php" method="get">
-            <input type="text" name="customer" />
+            <input type="text" name="search" placeholder="Search by first or last name" />
             <input type="submit" value="Search" />
         </form>
         <?php
